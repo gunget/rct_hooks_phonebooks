45a3a8daf040439b2bfc29a796aea47337a6bf2c
travis <aa@aa.com>
2020-12-05 19:53:03 +0900

    삭제기능 구현_완료

--- src/componets/FbReducer.jsx
+++ src/componets/FbReducer.jsx
@@ -2,15 +2,25 @@ import { useRef, useState } from "react";
 
 const FbReducer = (Fbooks, { type, payload }) => {
   console.log("reducer 실행됨");
-  const _idx = payload.id;
+
   switch (type) {
     case "SET_INIT_DATA":
       return { idx: 2, information: payload, search: "" };
 
     case "ADD_Fb_DATA":
+      const _idx = payload.id;
       return {
         idx: _idx,
         information: [...Fbooks.information, payload],
+        search: "",
+      };
+    case "DEL_Fb_DATA":
+      const _Fbooks = Fbooks.information.filter((obj) => {
+        return obj.id !== +payload;
+      });
+      return {
+        idx: Fbooks.idx,
+        information: _Fbooks,
         search: "",
       };
   }
--- src/componets/PhoneInfo.jsx
+++ src/componets/PhoneInfo.jsx
@@ -1,21 +1,28 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import { FbContext } from "./FbStore";
 
 const PhoneInfo = ({ data }) => {
   // props로 받음
+  console.log("--data", data);
+  const { dispatch } = useContext(FbContext);
+
+  const PbRemove = (e) => {
+    e.preventDefault();
+    dispatch({ type: "DEL_Fb_DATA", payload: e.target.parentNode.dataset.id });
+  };
+
   const style = {
     backgroundColor: "skyblue",
     width: "100%",
+    opacity: "0.85",
   };
 
   return (
-    <div style={style}>
-      <div>
-        {data.name} {data.number}
-        <span> </span>
-        <button>삭제</button>
-        <button>수정</button>
-      </div>
+    <div style={style} data-id={data.id}>
+      {data.name} {data.number}
+      <span> </span>
+      <button onClick={PbRemove}>삭제</button>
+      <button>수정</button>
     </div>
   );
 };
