7d8ddb4532fd5452922466a6230d5f67831dd949
Travis <aa@aa.net>
2020-12-07 16:52:22 +0900

    검색기능 구현중-reducer indexof에서 막힘

--- src/componets/FbReducer.jsx
+++ src/componets/FbReducer.jsx
@@ -9,7 +9,7 @@ const FbReducer = (Fbooks, { type, payload }) => {
         return { ...obj, editing: false }; //fetch해온 obj에 필요한 값 추가하기
         // editing이란 속성을 개별 입력값마다 집어넣어줌
       });
-      return { idx: 2, information: _information, search: "" };
+      return { idx: 2, information: _information, search: [] };
 
     case "ADD_Fb_DATA":
       const _idx = payload.id;
@@ -42,6 +42,15 @@ const FbReducer = (Fbooks, { type, payload }) => {
       editingObj.editing = !editingObj.editing;
       return { ...Fbooks }; //반드시 return하는 state필요
     //안의 일부 내용을 바꿔 새로운 객체를 선언해서 반환하는 것이기에 불변성 준수
+
+    case "SEARCH_NAME":
+      const {information} = Fbooks;
+      console.log('search name 리듀서', information, 'payload', payload);
+      const searchedObj = information.filter((obj) => 
+      obj.name.indexof(payload) !== -1 //검색어가 있는 것의 인덱스가 반환됨. 인덱스가 있으면 해당인덱스가 반환되어 결과는 true, 없으면 -1이 반환되어 결과는 false. 최종적으로 true인 것만 필터링 되어 나옴
+      );
+      console.log('search name 리듀서 검색결과', searchedObj);
+      return { ...Fbooks, search:searchedObj }; 
   }
 };
 
--- src/componets/Search.jsx
+++ src/componets/Search.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import {useRef, useContext, useEffect} from "react";
+import {FbContext} from './FbStore';
 
 const Search = () => {
   console.log("Search 실행");
+  const {dispatch} = useContext(FbContext);
+  const searchRef = useRef(false);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    dispatch({type:'SEARCH_NAME', payload:searchRef.current.value});
+  }
+  
+  // useEffect(() => {
+  //   dispatch({type:'SEARCH_NAME', payload:searchRef.current.value});
+  // }, []);
+
   return (
     <form>
-      <input type="text" placeholder="검색어"></input>
-      <button>입력</button>
+      <input type="text" placeholder="검색어" ref={searchRef}></input>
+      <button onClick={handleSearch}>이름검색</button>
     </form>
   );
 };
