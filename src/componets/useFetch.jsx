462af4ff17cd79728291a9b80cf809084611e79b
Travis <aa@aa.net>
2020-12-18 16:46:09 +0900

    장고 API서버에서 자료를 받아오는 것 까진 성공

--- src/componets/useFetch.jsx
+++ src/componets/useFetch.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 const useFetch = (callback) => {
   const [loading, setLoading] = useState(true);
@@ -17,8 +18,44 @@ const useFetch = (callback) => {
       },
     ];
 
+    axios.defaults.xsrfCookieName = "csrftoken";
+    axios.defaults.xsrfHeaderName = "X-CSRFToken"; //웹사이트 해킹 방지용도
+
+    // 기본적으로 axios.get(url), axios.post(url, data), axios.delete(url/id)를 통해서 쉽게 http 요청을 할 수 있습니다. 아주 좋은 모듈이죠?
+
+    const fetchInitData = async () => {
+      try {
+        const response = await axios.get("http://127.0.0.1:8000/api/fbooks");
+        const datum = await response.data[0];
+        console.log("받아온 데이터:", datum);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    // async function getPost() {
+    //   try {
+    //     const response = await axios.get("http://example.com/posts/12345/");
+    //     console.log(response);
+    //   }
+    //   catch (error) {
+    //     console.log(error);
+    //   }
+    // }
+
+    // const fetchInitData = async () => {
+    //   setLoading(true);
+    //   const response = await fetch(url);
+    //   const todoDatum = await response.json();
+    //   // await가 있어야 json형태로 다 바꿔준 다음에 반환한다. 없으면 다 변환되기 전에 반환해서 promise객체가 반환된다
+    //   const todoDatum_min = todoDatum.slice(0, 10);
+    //   callback({ type: "SET_INIT_DATA", payload: todoDatum_min });
+    //   // callback(todoDatum_min);
+    // };
+
     const fbFromLS = JSON.parse(localStorage.getItem("Fbooks"));
     //fetch로 받아올 경우 이부분을 수정해야 함
+    fetchInitData();
 
     if (fbFromLS !== null) {
       callback({ type: "SET_INIT_DATA", payload: fbFromLS });
